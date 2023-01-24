a0dcbbbcb48d16087e43f76b8152cfd67bdb7784
josefinagarcia0 <josefiina.garcia@gmail.com>
2023-01-23 23:11:24 -0300

    core components

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is my new App!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput 
+        style={styles.input} 
+        placeholder='add a new task' 
+        autoCorrect={false}
+        autoCapitalize='none'
+        />
+        <Button title='Add' color='#626893' />
+      </View>
     </View>
   );
 }
@@ -13,8 +20,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ccc',
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 50,
+    marginHorizontal: 20,
+  },
+  input: {
+    width: '75%',
+    borderBottomColor: '#626893',
+    borderBottomWidth: 1,
+    height: 40,
+    color: '#212121'
+  }
+
 });
